84cfa5483d25ab2355b33374746f5325a775b687
Garrett Kucinski <garrettryan@doejo.com>
2017-03-26 16:13:22 -0500

    commit shortcodes changes, added do_shortcode to content

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -16,7 +16,7 @@ function show_coach_list( $atts, $content = null ) {
     $no_whitespaces = preg_replace( '/\s*,\s*/', ',', filter_var( $attributes['coach_ids'], FILTER_SANITIZE_STRING ) );
     $coach_ids = explode( ',', $no_whitespaces );
 
-    if ( $attributes['coach_ids'] !== '' ) {
+    if ( '' !== $attributes['coach_ids'] ) {
         $options = array(
             'post_type' => $attributes['type'],
             'post__in' => $coach_ids,
@@ -37,7 +37,7 @@ function show_coach_list( $atts, $content = null ) {
         $output .= '<div class="coach-image-mask" style="background-image: url(\'' . get_the_post_thumbnail_url() . '\');"></div>';
             $output .= '<span class="coaches-bio">';
                 $output .= '<h5 class="coach-name">' . get_the_title() . '</h5>';
-                $output .= '<span class="coach-bio">' . get_the_content() . '</span>';
+                $output .= '<span class="coach-bio">' . do_shortcode( get_the_content() ) . '</span>';
             $output .= '</span>';
     $output .= '</div>';
         
