398a728e26c61cf77fbbf5adcbf36978b39d6611
Garrett Kucinski <garrettryan@doejo.com>
2017-03-25 22:46:24 -0500

    added logic to display all coaches if none are defined

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -1,43 +1,51 @@
 <?php
 
+// function to add logic for outputting coaces via shortcode
 function show_coach_list( $atts, $content = null ) {
     ob_start();
 
-   $attributes = shortcode_atts(
+    $attributes = shortcode_atts(
         array(
-                'type' => 'coaches',
-                'coach_ids' => '',
-            ), $atts
-        );
+            'type' => 'coaches',
+            'coach_ids' => '',
+        ), $atts
+    );
 
-        global $post;
+    global $post;
 
-        $no_whitespaces = preg_replace( '/\s*,\s*/', ',', filter_var( $attributes['coach_ids'], FILTER_SANITIZE_STRING ) );
-        $coach_ids = explode( ',', $no_whitespaces );
+    $no_whitespaces = preg_replace( '/\s*,\s*/', ',', filter_var( $attributes['coach_ids'], FILTER_SANITIZE_STRING ) );
+    $coach_ids = explode( ',', $no_whitespaces );
 
+    if ( $attributes['coach_ids'] !== '' ) {
         $options = array(
             'post_type' => $attributes['type'],
             'post__in' => $coach_ids,
         );
-
-        $query = new WP_Query( $options );
-
-        $output = '';
-        
-       if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-       $output .= '<div class="coach">';
-            $output .= '<div class="coach-image-mask" style="background-image: url(\'' . get_the_post_thumbnail_url() . '\');"></div>';
-                $output .= '<span class="coaches-bio">';
-                    $output .= '<h5 class="coach-name">' . get_the_title() . '</h5>';
-                    $output .= '<span class="coach-bio">' . get_the_content() . '</span>';
-                $output .= '</span>';
-        $output .= '</div>';
-        
-        endwhile;
-        endif;
-        
-        wp_reset_postdata();
-
-        return html_entity_decode( $output );
+    } else {
+        $options = array(
+            'post_type' => $attributes['type'],
+            'post_per_page' => -1,
+        );
     }
-    add_shortcode( 'coaches', 'show_coach_list' );
+
+    $query = new WP_Query( $options );
+
+    $output = '';
+        
+    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
+    $output .= '<div class="coach">';
+        $output .= '<div class="coach-image-mask" style="background-image: url(\'' . get_the_post_thumbnail_url() . '\');"></div>';
+            $output .= '<span class="coaches-bio">';
+                $output .= '<h5 class="coach-name">' . get_the_title() . '</h5>';
+                $output .= '<span class="coach-bio">' . get_the_content() . '</span>';
+            $output .= '</span>';
+    $output .= '</div>';
+        
+    endwhile;
+    endif;
+        
+    wp_reset_postdata();
+
+    return html_entity_decode( $output );
+}
+add_shortcode( 'coaches', 'show_coach_list' );
